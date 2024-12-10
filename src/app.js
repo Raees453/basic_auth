2c471b4989afe8e2ad7486c01b7c43305453041d
Raees453 <raeesali453@gmail.com>
2024-12-10 18:15:10 +0500

    Added morgan for logs

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ require('dotenv').config();
 const globalErrorHandler = require('./utils/global_error_handler');
 
 const routes = require('./routes');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(routes);
 
 app.use(globalErrorHandler);
